abc9921fe1352f3b8a03c00061c200215b91b365
CASL0 <gin.0046.822@gmail.com>
2023-10-21 01:42:58 +0900

    初期プロジェクトをコミット

---
+++ cronet/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "io.github.casl0.cronet"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 23
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    testImplementation(libs.org.hamcrest.all)
+}
